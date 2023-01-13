028c537edeb096aa61923a4adeb82626cd4add74
Kiktor The Wania <79936201+KiktorWania@users.noreply.github.com>
2023-01-13 08:56:49 +0100

    Update Main.java

--- ZadaniaDomowe/Builder/Main.java
+++ ZadaniaDomowe/Builder/Main.java
@@ -1,1 +1,11 @@
+package builder;
 
+public class Main {
+    public static void main(String args[]){
+        Builder ksiazka1 = new Builder.ChildBuilder("Pan Tadeusz", "Adam Mickiewicz").liczbaStron(100).cena(20).obiekt();
+        ksiazka1.show();
+        
+        Builder ksiazka2 = new Builder.ChildBuilder("Potop", "Henryk Sienkiewicz").obiekt();
+        ksiazka2.show();
+    }
+}
