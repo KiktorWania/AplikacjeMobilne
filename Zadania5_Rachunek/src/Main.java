dad035b270ba0aa7540e7c69d1f4c5023b21fa37
Kiktor The Wania <79936201+KiktorWania@users.noreply.github.com>
2023-01-13 08:03:12 +0100

    Update Main.java

--- Zadania5_Rachunek/src/Main.java
+++ Zadania5_Rachunek/src/Main.java
@@ -1,19 +1,22 @@
+package bank;
+
 public class Main {
     public static void main(String[] args) {
-        Osoba osoba1 = new Osoba("Wiktor", "Kania");
-        Osoba osoba2 = new Osoba("Andrzej", "Szklankowski");
-
-        Rachunek rachunek1 = osoba1.otworzRachunek();
-        Rachunek rachunek2 = osoba2.otworzRachunek();
-
-        rachunek1.info();
-        rachunek2.info();
-
-        rachunek1.wplac(200);
-        rachunek1.przelej(rachunek2, 100);
-        rachunek2.wyplac(50);
-
-        rachunek1.info();
-        rachunek2.info();
+        Osoba o1 = new Osoba("Wiktor", "Kania");
+        Osoba o2 = new Osoba("Andrzej", "Szklankowski");
+        
+        Rachunek r1 = o1.otworzRachunek();
+        
+       r1.metody.wplac(200);
+       r1.metody.wyplac(100);
+       
+       Rachunek r2 = o2.otworzRachunek();
+       
+       r1.metody.przelej(r2, 50);
+       
+       r1.metody.info();
+       r2.metody.info();
+        
     }
+    
 }
