089fc99a280a1f33250947eede2df0866b33ffd4
Kiktor The Wania <79936201+KiktorWania@users.noreply.github.com>
2023-01-13 08:02:54 +0100

    poprawka

--- Zadania5_Rachunek/src/Rachunek.java
+++ Zadania5_Rachunek/src/Rachunek.java
@@ -1,11 +1,16 @@
+package bank;
+
 public class Rachunek {
     private Osoba osoba;
+    public Metody metody;
     private float stanKonta;
 
     private int nr_konta;
 
     public Rachunek(Osoba osoba){
         this.osoba = osoba;
+        
+       metody = new Metody();
         stanKonta = 0;
 
         nr_konta = (int) Math.floor(Math.random() * (999999999 + 1));
@@ -13,6 +18,9 @@ public class Rachunek {
         System.out.println("Utworzono rachunek " + osoba.getImie() + " " + osoba.getNazwisko() + " " + nr_konta);
     }
 
+public class Metody{
+    public Metody(){}
+    
     public void wplac(float ile){
         stanKonta += ile;
         System.out.println("Wpłacono: " + ile);
@@ -25,8 +33,8 @@ public class Rachunek {
             System.out.println("Niewystarczająca ilość salda do przeprowadzenia tej operacji");
         }
     }
-
-    public void przelej(Rachunek naRachunek, int ile){
+    
+     public void przelej(Rachunek naRachunek, int ile){
         if(stanKonta >= ile){
             naRachunek.stanKonta += ile;
             stanKonta -= ile;
@@ -35,10 +43,11 @@ public class Rachunek {
             System.out.println("Niewystarczająca ilość salda do przeprowadzenia tej operacji");
         }
     }
-
+     
     public void info(){
         System.out.println("Saldo dla rachunku: " + osoba.getImie() + " " + osoba.getNazwisko() + " " + nr_konta + " wynosi: " + stanKonta);
     }
+}
 
     // Gettery i Settery aka. akcesory
 
